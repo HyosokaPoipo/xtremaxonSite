82c91f852a88d1c3b914b7eb234306989cd1f164
Maryadi <maryadi@elephanttalk.com>
2016-06-30 22:15:29 +0700

    nomor 5 setengahnya okiee... :D

--- XTremax/Program.cs
+++ XTremax/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,64 +11,106 @@ namespace FriendConsole
     public class Program
     {
         public static List<Person> personList = null;
+        private static List<Person> personHopsTable = new List<Person>();
 
         static void Main(string[] args)
         {
             Person personA = GetCurrentPerson("Person A");
             Person personD = GetCurrentPerson("Person D");
             Person personF = GetCurrentPerson("Person F");
+            Person personC = GetCurrentPerson("Person C");
+
 
             personList = new List<Person>();
-            int minimumNuberOfHops = FindMinimumNumberOfHops(personA, personD);
+            int minimumNuberOfHops = FindMinimumNumberOfHops(personF, personD);
             Console.WriteLine("Minimum number of Hops:" + minimumNuberOfHops);
 
             personList = new List<Person>();
-            int noFriends = FindNoFriendsCoveredInXHops(personF, 2);
+            int noFriends = FindNoFriendsCoveredInXHops(personA, 2);
             Console.WriteLine("Number of  Friends Covered in: " + noFriends);
 
-
+            //viewFriends(personA);
+            Console.WriteLine("Isi personHopsTable :");
+            foreach (Person test in personHopsTable) Console.WriteLine(test.ToString());
             Console.ReadKey();
         }
 
 
         public static int FindNoFriendsCoveredInXHops(Person a, int noHops)
         {
-            //Write your codes....
-            return 0;
+            personHopsTable.Clear();
+            for (int i = 0; i < a.Friends.Count; i++)
+              {
+                  if (!personHopsTable.Contains(a.Friends[i])) personHopsTable.Add(a.Friends[i]);
+                   countFriendsNum(a.Friends[i], noHops-1);                    
+             }
+
+            return personHopsTable.Count-1;
+        }
+
+        public static void countFriendsNum(Person a, int hops)
+        {
+            if (hops > 0)
+            {
+                for (int i = 0; i < a.Friends.Count; i++)
+                {
+                    if (!personHopsTable.Contains(a.Friends[i])) personHopsTable.Add(a.Friends[i]);
+                    countFriendsNum(a.Friends[i], hops-1);
+                }
+            }
+            
         }
 
 
         public static int FindMinimumNumberOfHops(Person a, Person b)
         {
-            int result = -1;
-            for (int i = 0; i < a.Friends.Count; i++)
-            {
-                if (a.Friends[i] == b) return result;
-                    
-                else 
+            int result = int.MaxValue;
+            
+            for (int i=0; i < a.Friends.Count; i++)            {
+                
+                if (a.Friends[i].ToString().Equals(b.ToString())) return 1;
+                else
                 {
-                    result += 1;
-                    return result + FindMinimumNumberOfHops((Person)a.Friends[i], b);}
-                //int temp = 1+FindMinimumNumberOfHops((Person)a.Friends[i], b);
-                //result = result < temp ? temp : result;
+                    Trace.WriteLine("nilai i dihops " + i);
+                    int temp = getMin(a.Friends[i], b, 1, a);
+                    if (temp < result) result = temp;
+                    
+                }
+                
             }
-           
             return result;
         }
 
         private static int getMin(Person a, Person b, int sum, Person prev)
         {
+            Trace.WriteLine("**************getMin Started**************");
+            Trace.WriteLine("Person a : "+a.ToString());
+            Trace.WriteLine("Person b : " + b.ToString());
+            Trace.WriteLine("Sum: " + sum);
+            Trace.WriteLine("Person prev : " + prev.ToString());
+            int result = int.MaxValue;
             for (int i = 0; i < a.Friends.Count; i++)
             {
+                Trace.WriteLine("i getMin "+i);
                 if (a.Friends[i] == prev) continue;
-                else if (a.Friends[i] == b) return sum;
+                else if (a.Friends[i].ToString().Equals(b.ToString())) return sum+1;
                 sum += 1;
-                getMin(a.Friends[i], b, sum, a);
+                int temp = getMin(a.Friends[i], b, sum, a);
+                if (temp < result) result = temp;
             }
 
-                return 0;
+            return result;
         }
 
+
+        public static void viewFriends(Person a)
+        {
+            for (int i = 0; i < a.Friends.Count; i++)
+            {
+                Console.WriteLine(a.Friends[i].ToString());
+            }
+
+        }
 
         public static Person GetCurrentPerson(String name)
         {
